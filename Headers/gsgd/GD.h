0912603e7b29eb494b38b58dda7606aa56c13e56
Nicola Pero <n.pero@mi.flashnet.it>
2002-07-08 17:33:34 +0000

    New file
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gsgd/trunk@14089 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gsgd/GD.h
@@ -0,0 +1,32 @@
+/* GD.h - Include all header for gsgd -*-objc-*-
+   Copyright (C) 2002 Free Software Foundation, Inc.
+   
+   Written by: Nicola Pero <n.pero@mi.flashnet.it>
+   July 2002
+
+   This file is part of the GNUstep GD Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/ 
+
+#ifndef _gsgd_GD_h__
+#define _gsgd_GD_h__
+
+#include <gsgd/GDFont.h>
+#include <gsgd/GDImage.h>
+#include <gsgd/GDLineStyle.h>
+
+#endif /* _gsgd_GDImage_h__ */
+
