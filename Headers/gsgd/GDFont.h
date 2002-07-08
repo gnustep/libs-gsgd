1e59851452c1bd5cedd23a63a58260710b7b95c9
Nicola Pero <n.pero@mi.flashnet.it>
2002-07-08 14:01:56 +0000

    New font class
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gsgd/trunk@14077 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gsgd/GDFont.h
@@ -0,0 +1,80 @@
+/* GDFont.h - Interface of GDFont
+   Copyright (C) 2002 Free Software Foundation, Inc.
+   
+   Written by: Nicola Pero <n.pero@mi.flashnet.it>
+   July 2002
+   
+   This file is part of the GNUstep GD Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/ 
+
+#ifndef _gsgd_GDFont_h__
+#define _gsgd_GDFont_h__
+
+#include <Foundation/NSObject.h>
+#include <gd.h>
+
+/*
+ * GDFont holds a fixed size fonts of - normally - 256 chars.
+ * There are five default fonts provided with libgd; you can create
+ * your own fixed size fonts if you need.  Details on how to do this
+ * can be found in the libgd sources.
+ */
+@interface GDFont : NSObject
+{
+  gdFont *_fontPtr;
+
+  /* If YES, we need to free _fontPtr when we are deallocated.  */
+  BOOL _needsToFreeFontPtr;
+}
+
+/*
+ * The default fonts.  
+ */
++ (GDFont *) tinyFont;
++ (GDFont *) smallFont;
++ (GDFont *) mediumBoldFont;
++ (GDFont *) largeFont;
++ (GDFont *) giantFont;
+
+/*
+ * Designated initializers.
+ */
+
+/* NB: Normally you are just happy with the default fonts - the designated
+ * initializers are for internal and/or advanced use.  */
+
+/* This does not copy fontPtr.  Use this initializer when fontPtr is a
+ * pointer to a statically allocated struct which is never freed.  */
+- (id) initWithFontPointerNoCopy: (gdFont *)fontPtr;
+
+/* This copies fontPtr.  Use this initializer when fontPtr is a
+ * pointer to malloc-ed data which might go away at any moment.  */
+- (id) initWithFontPointer: (gdFont *)fontPtr;
+
+/*
+ * Accessing the font pointer
+ */
+- (gdFont *)fontPointer;
+
+/* The width and height of each character in the font.  They all have the
+ * same size because the font is fixed size.  */
+- (int) width;
+- (int) height;
+
+@end
+
+#endif /* _gsgd_GDFont_h__ */
---
+++ Source/GDFont.m
@@ -0,0 +1,152 @@
+/* GDFont.m
+   Copyright (C) 2002 Free Software Foundation, Inc.
+   
+   Written by: Nicola Pero <n.pero@mi.flashnet.it>
+   July 2002
+   
+   This file is part of the GNUstep GD Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/ 
+
+#include <gsgd/GDFont.h>
+
+/* For gdFontSmall.  */
+#include <gdfonts.h>
+
+/* For gdFontTiny.  */
+#include <gdfontt.h>
+
+/* For gdFontMediumBold.  */
+#include <gdfontmb.h>
+
+/* For gdFontLarge.  */
+#include <gdfontl.h>
+
+/* Fot gdFontGiant.  */
+#include <gdfontg.h>
+
+static GDFont *tinyFont = nil;
+static GDFont *smallFont = nil;
+static GDFont *mediumBoldFont = nil;
+static GDFont *largeFont = nil;
+static GDFont *giantFont = nil;
+
+@implementation GDFont
+
+/*
+ * The default fonts.  
+ */
++ (GDFont *) tinyFont
+{
+  if (tinyFont == nil)
+    {
+      tinyFont = [[GDFont alloc] initWithFontPointerNoCopy: gdFontTiny];
+    }
+  return tinyFont;
+}
+
++ (GDFont *) smallFont
+{
+  if (smallFont == nil)
+    {
+      smallFont = [[GDFont alloc] initWithFontPointerNoCopy: gdFontSmall];
+    }
+  return smallFont;
+}
+
++ (GDFont *) mediumBoldFont
+{
+  if (mediumBoldFont == nil)
+    {
+      mediumBoldFont = [[GDFont alloc] initWithFontPointerNoCopy: 
+					 gdFontMediumBold];
+    }
+  return mediumBoldFont;
+}
+
++ (GDFont *) largeFont
+{
+  if (largeFont == nil)
+    {
+      largeFont = [[GDFont alloc] initWithFontPointerNoCopy: gdFontLarge];
+    }
+  return largeFont;
+}
+
++ (GDFont *) giantFont
+{
+  if (giantFont == nil)
+    {
+      giantFont = [[GDFont alloc] initWithFontPointerNoCopy: gdFontGiant];
+    }
+  return giantFont;
+}
+
+
+/* 
+ * Designated initializers.
+ */
+- (id) initWithFontPointerNoCopy: (gdFont *)fontPtr
+{
+  _needsToFreeFontPtr = NO;
+  _fontPtr = fontPtr;
+  return self;
+}
+
+
+- (id) initWithFontPointer: (gdFont *)fontPtr
+{
+  size_t lengthOfData;
+
+  _needsToFreeFontPtr = YES;
+  _fontPtr = objc_malloc (sizeof (gdFont));
+  _fontPtr = fontPtr;
+
+  lengthOfData = sizeof (char) * _fontPtr->nchars * _fontPtr->w * _fontPtr->h;
+  _fontPtr->data = objc_malloc (lengthOfData);
+  memcpy (_fontPtr->data, fontPtr->data, lengthOfData);
+
+  return self;
+}
+
+- (void) dealloc
+{
+  if (_needsToFreeFontPtr)
+    {
+      objc_free (_fontPtr->data);
+      objc_free (_fontPtr);
+    }
+  [super dealloc];
+}
+
+- (gdFont *)fontPointer
+{
+  return _fontPtr;
+}
+
+- (int) width
+{
+  return _fontPtr->w;
+}
+
+- (int) height
+{
+  return _fontPtr->h;
+}
+
+@end
+
+
